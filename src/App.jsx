26697011d62c790428a63d7807ccdb55a3343598
jiteshrock2019 <jiteshjosi583@gmail.com>
2021-01-07 12:52:14 +0530

    admin lte commited

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Dashboard from "./Dashboard";
+import Footer from "./Footer";
+import Header from "./Header";
+import Menu from "./Menu";
+function App() {
+  return (
+    <div class="wrapper">
+        <Header />
+        <Menu />
+        <Dashboard />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
